b41382ec2c83da410641d8e3b78efcb125612991
ccaballero646 <cristhian.caballero.gini@gmail.com>
2014-04-05 17:26:30 -0400

    añadida la funcionalidad de perfil de usuario

--- principal/models.py
+++ principal/models.py
@@ -4,10 +4,18 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    direccion=models.CharField(max_length=50)
+    telefono=models.CharField(max_length=15)
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
+
 
 class Proyecto(models.Model):
     usuario = models.ForeignKey(User,related_name='proyectos' )
     nombre=models.CharField(max_length=50)
+
 
 
     def __unicode__(self):
