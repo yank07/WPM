5c46d9f1ba9ef376ab748ce9fdead32784675107
rodrigo <rodri.valdez@gmail.com>
2014-04-05 08:37:23 -0400

    Configuracion inicial 2

--- principal/admin.py
+++ principal/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from principal.models import *
 
 # Register your models here.
+
+class ProyectoAdmin(admin.ModelAdmin):
+    list_display = ["nombre"]
+    search_fields = ["nombre"]
+
+admin.site.register(Proyecto, ProyectoAdmin)
+
+
+
+
+
+
+
--- principal/models.py
+++ principal/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+
+class Proyecto(models.Model):
+    usuario = models.ForeignKey(User,related_name='proyectos' )
+    nombre=models.CharField(max_length=50)
+
+
+    def __unicode__(self):
+        return unicode(self.nombre)
+
+
+class Fase(models.Model):
+    proyecto = models.ForeignKey(User,related_name='fases' )
+    nombre=models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return unicode(self.nombre)
+
+
+
