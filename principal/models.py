b61fa9a6474fa9e4a978a75d3af1bfeaec933ab6
ccaballero646 <cristhian.caballero.gini@gmail.com>
2014-04-09 09:56:40 -0400

    Resuelto issue #1

--- principal/models.py
+++ principal/models.py
@@ -14,13 +14,20 @@ class UserProfile(models.Model):
     activo=models.BooleanField(default=True)
 
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
+#
+# class UserProfile(models.Model):
+#     user = models.ForeignKey(User, unique=True)
+#     direccion=models.CharField(max_length=50)
+#     telefono=models.CharField(max_length=15)
+#
+# User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
 
-class UserProfile(models.Model):
-    user = models.ForeignKey(User, unique=True)
-    direccion=models.CharField(max_length=50)
-    telefono=models.CharField(max_length=15)
 
-User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
+class EstadosProyecto(models.Model):
+    descripcion = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return unicode(self.nombre)
 
 
 class Proyecto(models.Model):
